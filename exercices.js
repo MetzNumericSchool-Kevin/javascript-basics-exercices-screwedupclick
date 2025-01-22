064bea897737f2ed702f42ad6a07884563c24bd4
DuanrA <arnaudscrivo.dev@proton.me>
2025-01-22 11:16:08 +0100

    exo JS

--- exercices.js
+++ exercices.js
@@ -10,7 +10,12 @@ let boutique_ouverte = true;
 let boutique_fermee = false;
 
 let choix = prompt("Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? (fait un choix entre 1 et 4)");
-console.log("Quel est ton choix" + choix + " :" );
+console.log("Quel est ton choix" + choix + " : " );
+
+let choix_1 = nom_boutique;
+let choix_2 = nom_sorcier;
+let choix_3 = price;
+let choix_4 = healthpotion;
 
 if (choix_1) {
     console.log("La boutique s'appelle" + nom_boutique + " !");
@@ -24,6 +29,30 @@ else if (choix_3) {
 else if (choix_4) {
     console.log("Nous avons" + healthpotion + " en stock !");
 }
+
+switch (choix) {
+    case choix_1:
+      // Instructions à exécuter lorsque le résultat
+      // de l'expression correspond à valeur1
+    console.log("Le nom de la boutique est" + nom_boutique + " !");
+    break;
+    case choix_2:
+      // Instructions à exécuter lorsque le résultat
+      // de l'expression correspond à valeur2
+    console.log("Votre nom est " + nom_sorcier + " !");
+    break;
+    case choix_3:
+      // Instructions à exécuter lorsque le résultat
+      // de l'expression à valeurN
+    console.log(`Le prix unitaire d'un potion est de ${price} PO !`);
+    break;
+    case choix_4:
+      // Instructions à exécuter lorsque le résultat
+      // de l'expression correspond à valeur1
+    console.log("Nous avons" + healthpotion + " en stock !");
+    break;
+}
+
 
 
 /// CALCUL PRIX POTION
